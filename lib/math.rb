be464d984d22924e9adc39801ac63de150300c32
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-03-27 06:00:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,29 @@
 def addition(num1, num2)
-add=num1+num2
-return add
+  add=num1+num2
+  return add
 end
 
 def subtraction(num1, num2)
-sub=num1-num2
-return sub
+  sub=num1-num2
+  return sub
 end
 
 def division(num1, num2)
-div=num1/num2
-return div
+  div=num1/num2
+  return div
 end
 
 def multiplication(num1, num2)
-mul= num1*num2
-return mul
+  mul= num1*num2
+  return mul
 end
 
 def modulo(num1, num2)
-mod=num1%num2
-return mod
+  mod=num1%num2
+  return mod
 end
 
 def square_root(num)
-
+  sr=num**
+  return sr
 end
